18c2d41945d583ec3c28ddebb27cd59ba6ad5362
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2024-05-23 02:39:12 +0600

    completed the split functionality

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ const split = (dir, btn) => {
     ? (parent.style.flexDirection = "row")
     : (parent.style.flexDirection = "column");
 };
+
+// Creating div for btnBlocks
+const btnBlock1 = document.createElement("div");
+const btnBlock2 = document.createElement("div");
+
+// Giving them btnWrapper Class
+btnBlock1.className = "btnWrapper";
+btnBlock2.className = "btnWrapper";
+
+//Setting innerHTML for btnBlocks
+btnBlock1.innerHTML = `
+    <button class="split-btn" onclick="split('vertical', this)">V</button>
+    <button class="split-btn" onclick="split('horizontal', this)">H</button>
+    `;
+
+btnBlock2.innerHTML = `
+    <button class="split-btn" onclick="split('vertical', this)">V</button>
+    <button class="split-btn" onclick="split('horizontal', this)">H</button>
+    `;
+
+// Made parent div empty and appended btnBlocks to it
+
+parent.innerHTML = "";
+parent.appendChild(btnBlock1);
+parent.appendChild(btnBlock2);
